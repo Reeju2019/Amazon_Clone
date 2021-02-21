9481fb0904bb6a793fba9c636eea5db2ae5f6dee
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-02-22 00:09:10 +0530

    Update App.js

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon Clone</h1>
+    <div className="app">
       <Header />
-      {/* Home */}
+      <Home />
     </div>
   );
 }
