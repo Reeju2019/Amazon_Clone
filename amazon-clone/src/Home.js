5e9d9af5482af75413c21575e0a4dd5cb2f3b504
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-02-22 00:16:58 +0530

    Update Home.js

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -4,7 +4,12 @@ import "./Home.css";
 function Home() {
   return (
     <div>
-      <div className></div>
+      <div className="home">
+        <h1>I'm in the Home Component</h1>
+        <div className="home__container">
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
