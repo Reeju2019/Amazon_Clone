d8279ab53a9e0604bc29a8f80322a67d482afebf
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-02-22 11:24:25 +0530

    adding the fade out effect on banner

--- amazon-clone/src/Home.js
+++ amazon-clone/src/Home.js
@@ -1,13 +1,32 @@
 import React from "react";
+import Product from "./Product";
 import "./Home.css";
 
 function Home() {
   return (
     <div>
       <div className="home">
-        <h1>I'm in the Home Component</h1>
         <div className="home__container">
-          <img src="" alt="" />
+          <img
+            className="home__image"
+            src="https://images-eu.ssl-images-amazon.com/images/G/31/AmazonVideo/2020/X-site/Multititle/Bollywood/1500x600_Hero-Tall_np_bolly._CB405289994_.jpg"
+            alt="Banner"
+          />
+
+          <div className="home__row">
+            <Product />
+            <Product />
+          </div>
+
+          <div className="home__row">
+            <Product />
+            <Product />
+            <Product />
+          </div>
+
+          <div className="home__row">
+            <Product />
+          </div>
         </div>
       </div>
     </div>
