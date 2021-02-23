5d53304226c387d24f3729ac84b8615fe9b1313a
Reeju Bhattacherji <reejubhattacherji@gmail.com>
2021-02-23 21:55:54 +0530

    creating the subtotal box in checkout page

--- amazon-clone/src/Subtotal.js
+++ amazon-clone/src/Subtotal.js
@@ -5,8 +5,26 @@ import CurrencyFormat from "react-currency-format";
 function Subtotal() {
   return (
     <div className="subtotal">
-      <h1>Sub-Total:</h1>
-      <CurrencyFormat />
+      <CurrencyFormat
+        renderText={(value) => (
+          <>
+            <p>
+              Subtotal (0 items):<strong>0</strong>
+            </p>
+            <small className="subtotal__gift">
+              <input type="checkbox" />
+              This order contains a gift
+            </small>
+          </>
+        )}
+        decimalScale={2}
+        value={0}
+        displayType={"text"}
+        thousandSeparator={true}
+        prefix={"$"}
+      />
+
+      <button className="subtotal__button">Proceed to Checkout</button>
     </div>
   );
 }
